8bdec71e449f7b14b048d72d9d4a6119edb3e604
Sérgio <sergioriosribeiro@outlook.com>
2024-05-08 11:26:00 -0300

    Cria projeto

---
+++ server/src/config/openai.js
@@ -0,0 +1,22 @@
+import OpenAI from "openai";
+
+module.exports = class openai {
+    static configuration(){
+        const openai = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY,
+          });
+          return openai
+    }
+
+    static textCompletion({prompt}){
+        return{
+            model: "gpt-3.5-turbo-instruct",
+            prompt: `${prompt}`,
+            temperature: 0.11,
+            max_tokens: 3500,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+          }
+    }
+}
